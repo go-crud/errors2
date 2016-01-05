dd02bfbdeececb5f24957d71a138549c2e1889e9
lshengjian <lsj178@139.com>
2016-01-05 13:52:52 +0800

    init by alex

---
+++ error.go
@@ -0,0 +1,124 @@
+package errors2
+
+//https://raw.githubusercontent.com/plimble/utils/master/errors2/error.go
+
+const (
+	Internal = iota
+	BadReq
+	NotFound
+	Unauthorized
+	Forbidden
+)
+
+type Error interface {
+	HttpStatus() int
+	Error() string
+	Type() int
+}
+
+type Errors2 struct {
+	httpStatus int    `json:"-"`
+	Err        string `json:"error,omitempty"`
+	Message    string `json:"message"`
+	ty         int    `json:"-"`
+}
+
+func Sql(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	switch err.Error() {
+	case "sql: no rows in result set":
+		return NewNotFound("not found")
+	}
+
+	return NewInternal(err.Error())
+}
+
+func Mgo(err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if err.Error() == "not found" {
+		return NewNotFound("not found")
+	}
+
+	return NewInternal(err.Error())
+}
+func (err *Errors2) Error() string {
+	return err.Message
+}
+
+func (err *Errors2) Title() string {
+	return err.Err
+}
+
+func (err *Errors2) HttpStatus() int {
+	return err.httpStatus
+}
+
+func (err *Errors2) Type() int {
+	return err.ty
+}
+
+func NewAnyError() error {
+	return NewError(500, "", "any error", Internal)
+}
+
+func NewError(status int, code, msg string, ty int) *Errors2 {
+	return &Errors2{status, "", msg, ty}
+}
+
+func NewInternal(msg string) *Errors2 {
+	return &Errors2{500, "", msg, Internal}
+}
+
+func NewBadReq(msg string) *Errors2 {
+	return &Errors2{400, "", msg, BadReq}
+}
+
+func NewNotFound(msg string) *Errors2 {
+	return &Errors2{404, "", msg, NotFound}
+}
+
+func NewUnauthorized(msg string) *Errors2 {
+	return &Errors2{401, "", msg, Unauthorized}
+}
+
+func NewForbidden(msg string) *Errors2 {
+	return &Errors2{403, "", msg, Forbidden}
+}
+
+//With err code
+
+func NewErrorCode(status int, code, msg string, ty int) *Errors2 {
+	return &Errors2{status, code, msg, ty}
+}
+
+func NewInternalCode(code, msg string) *Errors2 {
+	return &Errors2{500, code, msg, Internal}
+}
+
+func NewBadReqCode(code, msg string) *Errors2 {
+	return &Errors2{400, code, msg, BadReq}
+}
+
+func NewNotFoundCode(code, msg string) *Errors2 {
+	return &Errors2{404, code, msg, NotFound}
+}
+
+func NewUnauthorizedCode(code, msg string) *Errors2 {
+	return &Errors2{401, code, msg, Unauthorized}
+}
+
+func NewForbiddenCode(code, msg string) *Errors2 {
+	return &Errors2{403, code, msg, Forbidden}
+}
+
+func Panic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
